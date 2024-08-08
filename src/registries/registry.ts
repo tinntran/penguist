75bf986bd53dcf67de186ba6c2906e22c47a476a
tinntran <tinn6w6@gmail.com>
2024-08-08 10:42:50 +0700

    store registries in memory instead of session storage

--- src/registries/registry.ts
+++ src/registries/registry.ts
@@ -1,27 +1,14 @@
 export class Registry<T> {
   protected records: Record<string, T> = {}
 
-  constructor(readonly kind: string) {
-    this.storeAtSessionStorage({})
-  }
-
-  protected storeAtSessionStorage(data?: typeof this.records) {
-    sessionStorage.setItem(this.kind, JSON.stringify(data ? data : this.records))
-  }
+  constructor(readonly kind: string) {}
 
   register(key: string, value: T) {
     this.records[key] = value
-    this.storeAtSessionStorage()
   }
 
   get(key: string) {
-    const item = sessionStorage.getItem(this.kind)
-
-    if (item) {
-      return (JSON.parse(item) as typeof this.records)[key]
-    }
-    
-    return null
+    return this.records[key]
   }
 
   getAll() {
@@ -29,7 +16,7 @@ export class Registry<T> {
   }
 
   clear() {
-    sessionStorage.removeItem(this.kind)
+    this.records = {} 
   }
 }
 
