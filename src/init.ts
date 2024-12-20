926c84e8ef5a9d359fda11f2075ff469262a174b
tinntran <tinn6w6@gmail.com>
2024-12-20 21:31:34 +0700

    add a silly log when initialize penguist

--- src/init.ts
+++ src/init.ts
@@ -2,6 +2,10 @@ import { fade, rush } from './default/anims'
 import { Present, Slide } from './elements'
 
 export default function init() {
+  const eyes = ['\'\'', '"', '++', '**', '==', '..', '~~', '@@']
+
+  console.log(`Your penguin is ready to go!\n%c(${eyes[Math.floor(Math.random() * eyes.length)]})>`, 'font-size: 42px;')
+
   document.body.style.overflow = 'clip'
 
   customElements.define('p-present', Present)
